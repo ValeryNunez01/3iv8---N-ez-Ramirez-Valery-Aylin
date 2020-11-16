a00f670f1530bc587f24cbfadadbeb124c378201
ValeryNunez01 <72088086+ValeryNunez01@users.noreply.github.com>
2020-11-15 18:21:44 -0600

    Ejercicio2-15/11/20-Nuñez Valery
    Ejercicio2-15/11/20-Nuñez Valery

--- Principal.java
+++ Principal.java
@@ -1,10 +1,12 @@
 public class Principal{
-    public static void main(String[] args) {
-     
-        Edad llamar = new Edad();
-        System.out.println("\nMade by Valery Ayin Nunez Ramirez - 3IV8");
-        //Se llama al metodo creado en la clase Edad
-        llamar.ingresar();
 
+    public static void main(String[] args){
+
+        System.out.println("\nMade by Valery Ayin Nunez Ramirez - 3IV8\n");
+
+        Metodos llamar = new Metodos();
+
+        //Mandamos llamar al metodo menu
+        llamar.menu();
     }
 }
